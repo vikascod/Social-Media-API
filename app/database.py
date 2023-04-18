4d63a5e1cf90e3f90c61e29bfce2f99dc6b04a13
Vikas <vikas1618072@gmail.com>
2023-04-18 15:40:05 +0530

    fixed database issue

--- app/database.py
+++ app/database.py
@@ -3,7 +3,17 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from decouple import config
 
-engine = create_engine(config('DATABASE_URL'))
+PROVIDER = config('PROVIDER')
+POSTGRES_DB = config('POSTGRES_DB')
+POSTGRES_USER = config('POSTGRES_USER')
+POSTGRES_PASSWORD = config('POSTGRES_PASSWORD')
+POSTGRES_HOST = config('POSTGRES_HOST')
+POSTGRES_PORT = config('POSTGRES_PORT')
+
+DATABASE_URL = f"{PROVIDER}://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -15,3 +25,5 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
