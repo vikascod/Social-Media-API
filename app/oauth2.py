27684e86458ce30ce061ad294e8b4be57aef2dda
Vikas <vikas1618072@gmail.com>
2023-04-19 14:48:08 +0530

    hide credentials

--- app/oauth2.py
+++ app/oauth2.py
@@ -5,11 +5,12 @@ from fastapi.security import OAuth2PasswordBearer
 from app import schemas, models
 from sqlalchemy.orm import Session
 from app.database import get_db
+from decouple import config
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = config('SECRET_KEY', '09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7')
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
