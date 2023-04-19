791df8edba86bbf2b9c770b7cbdb7eb456fc5eb3
Vikas <vikas1618072@gmail.com>
2023-04-19 13:53:51 +0530

    testing

--- app/database.py
+++ app/database.py
@@ -3,15 +3,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from decouple import config
 
-PROVIDER = config('PROVIDER')
-POSTGRES_DB = config('POSTGRES_DB')
-POSTGRES_USER = config('POSTGRES_USER')
-POSTGRES_PASSWORD = config('POSTGRES_PASSWORD')
-POSTGRES_HOST = config('POSTGRES_HOST')
-POSTGRES_PORT = config('POSTGRES_PORT')
+# PROVIDER = config('PROVIDER')
+# POSTGRES_DB = config('POSTGRES_DB')
+# POSTGRES_USER = config('POSTGRES_USER')
+# POSTGRES_PASSWORD = config('POSTGRES_PASSWORD')
+# POSTGRES_HOST = config('POSTGRES_HOST')
+# POSTGRES_PORT = config('POSTGRES_PORT')
 
 
-DATABASE_URL = f"{PROVIDER}://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+# DATABASE_URL = f"{PROVIDER}://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+DATABASE_URL = "postgresql://postgres:Mohan12345!#New@db.sufzgnedhgkypnzigomx.supabase.co:6543/postgres"
 
 
 engine = create_engine(DATABASE_URL)
